be31500cd091311eb90283d8e0d7f49c75df728a
Ibrahim Duman <dumanibrahm@gmail.com>
2025-04-15 14:33:46 +0300

    Initial project setup with ASP.NET Core MVC completed.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlogManagementProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
